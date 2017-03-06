b5c9e54be31463e6347f8ed3c9897f56b2bf4048
Omid Khataee <maverickxm2@live.ca>
2017-03-06 10:40:02 -0500

    removed page1&page2

--- Keanu/src/app/app.component.ts
+++ Keanu/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-// Default Pages (Leave alone for now)
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 
 // Keanu Pages
 import { LoginPage } from '../pages/login/login';
