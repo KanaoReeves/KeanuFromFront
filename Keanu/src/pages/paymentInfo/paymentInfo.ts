43d281eb230c783ccc0166d6ad93d422f9d9c12a
Kevin <kevinburnside15>
2017-03-08 16:53:52 -0500

    Changed public adminRights: false; to public adminRights: boolean=false;...still getting a 401 error

--- Keanu/src/pages/paymentInfo/paymentInfo.ts
+++ Keanu/src/pages/paymentInfo/paymentInfo.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { DatePipe } from '@angular/common';
+import { LoginPage } from '../login/login';
 /*
   Generated class for the Login page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
@@ -17,13 +17,13 @@ export class PaymentInfoPage {
   //Info input on this page
   public address: any;
   public paymentInfo: any;
-  public adminRights: false;
+  public adminRights: boolean=false;
  //Info pushed from previous RegisterPage  
   public username: string;
   public password: string;
   public email: string;
   public DisplayName: any;
-  constructor(private navCtrl: NavController, private navParams: NavParams, private http: Http) {
+  constructor(private navCtrl: NavController, private navParams: NavParams, private http: Http, private alertCtrl: AlertController) {
     //Gets info from register page
     this.DisplayName=navParams.get('DisplayNameParam')    
     this.email=navParams.get('emailparam');
@@ -42,10 +42,8 @@ export class PaymentInfoPage {
       cardType: '',
       num: '',
       expiry: '1/1/17 12:00:00 AM UTC'
-    }   
-    this.adminRights= false;    
-  }    
-   
+    }          
+  }       
     public Register():void{
       let link = 'https://keanubackend.herokuapp.com/login/register'
       let data = JSON.stringify({'username': this.username, 'password': this.password, 'displayName': this.DisplayName,
@@ -56,9 +54,20 @@ export class PaymentInfoPage {
           data=>{
         
           }, 
-          err=>{
-                console.log(data);
-          });      
-    }
-  }
+          err => {
+              console.log("ERROR!: ", err);
+            },
+            ()=>{
+              console.log('posted registration done')
+              this.alertCtrl.create({
+                title: 'registration Successful',
+                subTitle: 'Congratulations, redirecting to the login page',
+                buttons: ['OK']
+              }).present();
+              this.navCtrl.setRoot(LoginPage);
+            }
+        );
+  }      
+}
+  
 
--- Keanu/src/pages/register/register.ts
+++ Keanu/src/pages/register/register.ts
@@ -5,7 +5,6 @@ import { PaymentInfoPage } from '../paymentInfo/paymentInfo';
 
 /*
   Generated class for the Login page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
