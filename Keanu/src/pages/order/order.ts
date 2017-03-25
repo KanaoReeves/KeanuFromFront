683c77ddf05d4eb62d59b87dbeef89c02fc29d50
Omid Khataee <maverickxm2@live.ca>
2017-03-25 08:38:52 -0400

    getting back a correct order

--- Keanu/src/pages/order/order.ts
+++ Keanu/src/pages/order/order.ts
@@ -3,25 +3,33 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import { CartService } from '../../services/cartService';
+import { ItemService } from '../../services/getItem';
 
 // I only post to the backend once the button is clicked on this page
 
 @Component({
   selector: 'page-order',
   templateUrl: 'order.html',
-  providers: [CartService]
+  providers: [CartService, ItemService]
 })
+
 export class OrderPage {
-  public cartItems: Array<Object>
+  public cartItems: Array<any>
+  public cartItem: { itemId: String, quantity: Number }
+  public orderItems: Array<any>
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public http: Http,
     private storage: Storage,
-    private cartService: CartService) {
-      this.cartItems = new Array<Object>();
+    private cartService: CartService,
+    private itemService: ItemService) {
+    this.cartItems = new Array<any>()
+    this.orderItems = new Array<any>()
   }
+
+  public getItems
 
   ionViewDidLoad() {
     console.log('In Order Page');
@@ -29,9 +37,19 @@ export class OrderPage {
     this.cartService.getCartAsObject().then(itemsData => {
       this.cartItems = itemsData;
 
-      
-      // Code can go in here
-      console.log(this.cartItems);
+      this.cartItems.forEach(element => {
+        this.cartItem = element;
+         this.itemService.GetItem(this.cartItem.itemId, this.cartItem.quantity).then((data) => {
+            this.orderItems.push(data);
+            console.log(this.orderItems);
+         })
+
+        /*
+          this.orderItems.push(data)
+          this.orderItem.push(", this.cartItem.quantity, )
+            console.log(this.orderItems);
+        })*/
+      })
     })
   }
 }
---
+++ Keanu/src/services/getItem.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ItemService {
+    public menuItem: {_id: String, calories: Number, category: String, description: String, imageURL: String, name: String, price: any, tags: Array<String>}
+    public orderItem: { name: String,  price: any, imageURL: String, quantity: Number }
+
+    constructor(private http: Http) {
+        this.orderItem = {name: "", price: 0, imageURL: "", quantity: 0}
+    }
+
+    public GetItem(id, quantity) {
+        return new Promise<Object>((resolve, reject) => {
+            let menu = this.http.get(`https://keanubackend.herokuapp.com/item/id/${id}`).map(res => res.json()).subscribe(
+                data => {
+                    this.menuItem = data.data.item;
+                    this.orderItem.name = this.menuItem.name;
+                    this.orderItem.price = this.menuItem.price * quantity;
+                    this.orderItem.imageURL = this.menuItem.imageURL;
+                    this.orderItem.quantity = quantity;
+                },
+                err => {
+                    console.log("Oops!");
+                    reject(null)
+                },
+                () => {
+                    resolve(this.orderItem);
+                }
+            );
+        })
+
+        //console.log("From GetMenu.ts " + this.menuItems);
+    }
+}
