b63d301c6af780a991a87deb4d363e2c49f51c0f
Omid Khataee <maverickxm2@live.ca>
2017-03-26 15:47:23 -0400

    Almost done

--- Keanu/src/pages/order/order.ts
+++ Keanu/src/pages/order/order.ts
@@ -17,7 +17,7 @@ export class OrderPage {
   public cartItems: Array<any>
   public cartItem: { itemId: String, quantity: Number }
   public orderItems: Array<{ name: String, price: any, imageURL: String, quantity: Number }>
-  public totalPrice: {subTotal: number, tax: number; total: number}
+  public totalPrice: { subTotal: number, tax: number; total: number }
   public paymentInformation: Object
 
   constructor(
@@ -30,11 +30,11 @@ export class OrderPage {
     private itemService: ItemService) {
     this.cartItems = new Array<any>()
     this.orderItems = new Array<any>()
-    this.totalPrice = {subTotal :0, tax: 0, total: 0} 
+    this.totalPrice = { subTotal: 0, tax: 0, total: 0 }
     this.paymentInformation = new Object
   }
 
-  public GetPaymentInfo():void {
+  public GetPaymentInfo(): void {
 
     this.storage.get('token').then(value => {
       let headers = new Headers();
@@ -45,17 +45,47 @@ export class OrderPage {
         data => {
           this.paymentInformation = data.data.paymentInfo;
           console.log(this.paymentInformation);
-      }, err => {
-        console.log(err);
-      },
+        }, err => {
+          console.log(err);
+        },
       )
     })
   }
 
-  public ConfirmOrder(){
-     alert("Your order is on its way");
-     
-  }
+  public ConfirmOrder() {
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    let newdate = day + ":" + month + ":" + year
+
+    alert("Your order is on its way");
+    let confirmOrder = {
+      "date": newdate,
+      "delivery": false,
+      "items": this.orderItems,
+      "price": this.totalPrice.total
+    }
+
+    this.storage.get('token').then(value => {
+    
+    let headers = new Headers();
+    headers.append('token', value)
+    
+    let options = new RequestOptions({
+      headers: headers,
+      body: JSON.stringify(confirmOrder)
+    })
+
+    this.http.get('https://keanubackend.herokuapp.com/order/add', options).map(res => res.json()).subscribe(
+        data => {
+          console.log(data);
+        }, err => {
+          console.log(err);
+        },
+      )
+  })
+}
 
   ionViewDidLoad() {
     console.log('In Order Page');
@@ -71,13 +101,12 @@ export class OrderPage {
         // Do this code refactor inside the next iteration
 
         // Create a http request with a header
-
         this.GetPaymentInfo();
 
         this.orderItems.forEach(element => {
           this.totalPrice.subTotal += element.price
         });
-        
+
         this.totalPrice.tax = (this.totalPrice.subTotal * .13)
         this.totalPrice.total = (this.totalPrice.subTotal + this.totalPrice.tax)
       });
