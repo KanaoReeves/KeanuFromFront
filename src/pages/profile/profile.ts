c00aa1b1163bcdebf053c2204d09cdbd2331354c
Omid Khataee <maverickxm2@live.ca>
2017-04-07 19:26:29 -0400

    shows profile and order

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Http, RequestOptions, Headers, } from '@angular/http';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
+import { CartService } from '../../services/cartService';
+import { ItemService } from '../../services/getItem';
 /*
   Generated class for the Profile page.
 
@@ -12,11 +16,89 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+  public paymentInformation: Object
+  public addressInformation: Object
+  public personalInformation: Object
+  public fullName: Object
+  public orders : Array<Object>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+    public request: RequestOptions,
+    private storage: Storage,
+    private alertCtrl: AlertController) {
+    this.paymentInformation = new Object
+    this.addressInformation = new Object
+    this.personalInformation = new Object
+    this.fullName = new Object
+    this.orders = new Array<Object>()
+  }
+
+  public GetAddressInfo(): void {
+
+    this.storage.get('token').then(value => {
+      let headers = new Headers();
+      headers.append('token', value)
+
+      let options = new RequestOptions({ headers: headers });
+      this.http.get('https://keanubackend.herokuapp.com/customer/profile', options).map(res => res.json()).subscribe(
+        data => {
+          this.addressInformation = data.data.user.address;
+          this.personalInformation = data.data.user;
+          this.fullName = data.data.user.displayName;
+          //console.log(this.personalInformation);
+        }, err => {
+          console.log(err);
+        },
+      )
+    })
+  }
+
+  public GetPaymentInfo(): void {
+
+    this.storage.get('token').then(value => {
+      let headers = new Headers();
+      headers.append('token', value)
+
+      let options = new RequestOptions({ headers: headers });
+      this.http.get('https://keanubackend.herokuapp.com/customer/payment', options).map(res => res.json()).subscribe(
+        data => {
+          this.paymentInformation = data.data.paymentInfo;
+          //console.log(this.paymentInformation);
+        }, err => {
+          console.log(err);
+        },
+      )
+    })
+  }
+
+  public GetOrderHistory(): void {
+     this.storage.get('token').then(value => {
+      let headers = new Headers();
+      headers.append('token', value)
+
+      let options = new RequestOptions({ headers: headers });
+      this.http.get('https://keanubackend.herokuapp.com/order', options).map(res => res.json()).subscribe(
+        data => {
+          this.orders = data.data.orders;
+          console.log(this.orders);
+        }, err => {
+          console.log(err);
+        },
+      )
+    })
+  }
+
+  public ShowOrderDetails(order: Object):void{
+    console.log(order); 
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+    this.GetAddressInfo();
+    this.GetPaymentInfo();
+    this.GetOrderHistory();
   }
 
 }
